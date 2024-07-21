b575e6e2eace7aec1cab8e9ccc94838fffa86733
GideokKim <ikid4127@naver.com>
2024-07-21 18:53:52 +0900

    feat: add grid, thread block, tread info print function

--- CudaTest/hello-cuda-world/kernel.cu
+++ CudaTest/hello-cuda-world/kernel.cu
@@ -6,14 +6,34 @@
 
 __global__ void hello_cuda() { printf("hello CUDA world \n"); }
 
-int main() {
-  int nx, ny;
-  nx = 16;
-  ny = 4;
-  dim3 block(8, 2);
-  dim3 grid(nx / block.x, ny / block.y);
+__global__ void printThreadsInfo() {
+  printf(
+      "gridDim.x: %d, gridDim.y: %d, gridDim.z: %d, blockIdx.x: %d, "
+      "blockIdx.y: %d, blockIdx.z: %d, threadIdx.x: %d, threadIdx.y: %d, "
+      "threadIdx.z: %d,\n",
+      gridDim.x, gridDim.y, gridDim.z, blockIdx.x, blockIdx.y, blockIdx.z,
+      threadIdx.x, threadIdx.y, threadIdx.z);
+}
 
-  hello_cuda<<<grid, block>>>();
+// int main() {
+//   int nx, ny;
+//   nx = 16;
+//   ny = 4;
+//   dim3 block(8, 2);
+//   dim3 grid(nx / block.x, ny / block.y);
+
+//   hello_cuda<<<grid, block>>>();
+//   cudaDeviceSynchronize();
+
+//   cudaDeviceReset();
+//   return 0;
+// }
+
+int main() {
+  dim3 block(2, 2, 2);
+  dim3 grid(4, 4, 4);
+
+  printThreadsInfo<<<grid, block>>>();
   cudaDeviceSynchronize();
 
   cudaDeviceReset();
