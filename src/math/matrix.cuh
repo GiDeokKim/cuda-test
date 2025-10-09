a767a69562b0de03e2cd377ab0d6163bb8f3202a
Gideok Kim <ikid4127@naver.com>
2025-10-09 19:17:17 +0900

    💚 Introduce bazel build system

--- src/math/matrix.cuh
+++ src/math/matrix.cuh
@@ -1,11 +1,12 @@
 #ifndef CUDA_TEST_MATRIX_H
 #define CUDA_TEST_MATRIX_H
+
 #include <cuda_runtime.h>
 
 #include <iostream>
 
 #include "device_launch_parameters.h"
 
-int MatrixMultiplication(int* M, int* N, int* P, int width);
+int MatrixMultiplication(int *M, int *N, int *P, int width);
 
-#endif  // CUDA_TEST_MATRIX_H
+#endif // CUDA_TEST_MATRIX_H
